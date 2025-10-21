b46dd01a7175e0cb1a5f6dd22769a5adfc2b469b
Rohit Singh Pal <rohit.pal7171@gmail.com>
2025-10-21 12:07:09 +0530

    fix: added env for security reasons

--- src/database/firebase/firebase.js
+++ src/database/firebase/firebase.js
@@ -1,18 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDdTvh5bbwVTJVRMLmqBaSVcDQBpnRBdPs',
-  authDomain: 'shivaay-library-init.firebaseapp.com',
-  projectId: 'shivaay-library-init',
-  storageBucket: 'shivaay-library-init.firebasestorage.app',
-  messagingSenderId: '336727591345',
-  appId: '1:336727591345:web:e86a704252c11d74169590',
-  // databaseURL: 'add real time database url here',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
